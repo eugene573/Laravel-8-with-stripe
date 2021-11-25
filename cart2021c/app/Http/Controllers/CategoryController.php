b3c8e7deae5596e12642f0230c435de9b5c7742c
eugene573 <eugenefong2002@gmail.com>
2021-11-25 21:41:14 +0800

    add

--- cart2021c/app/Http/Controllers/CategoryController.php
+++ cart2021c/app/Http/Controllers/CategoryController.php
@@ -4,15 +4,20 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use DB; //import Database Library
-use App\Models\Category; //import category model
+use App\Models\Category; //import category model 
 
 class CategoryController extends Controller
 {
     public function add(){
-        $r=request(); //received the data by GET or POST mothod $_POST['name']
+        $r=request();  //received the data by GET or POST mothod 
         $addCategory=Category::create([
             'name'=>$r->categoryName,
-        ]);  
-        Return view('addCategory');  
+        ]);
+        Return redirect()->route('showCategory');
+    }
+
+    public function view(){
+        $viewCategory=Category::all(); //generate SQL select * from categories
+        Return view('showCategory')->with('categories',$viewCategory);
     }
 }
--- cart2021c/resources/views/welcome.blade.php
+++ cart2021c/resources/views/welcome.blade.php
@@ -1,37 +1,33 @@
 @extends('layout')
 @section('content')
-    <div class="container-fluid">
-        <div class="row">
-            <div class="col-sm-12">
-                <a class="nav-link" href="{{url('/home')}}">
-                <img src="images/promotion_03.jpg" alt="" class="img-fluid" width=100%  > </a>
-            </div>                    
+<div class="container-fluid">
+    <div class="row">
+        <div class="col-sm-12">
+            <a class="nav-link" href="{{url('/home')}}"><img src="{{ asset('images/promotion_03.jpg')}}"  alt="" class="img-fluid" width=100%  > </a>
+        </div>                    
+    </div>        
+    <div class="row" style="margin-top:20px;">
+        <div class="col-sm-4" style="text-align: center;">
+            <img src="{{ asset('images/samsungPhone.jpg')}}
+" width=50% alt="" class="img-fluid" > 
+            <p>SAMSUNG</p>
         </div>
-                
-
-        <div class="row" style="margin-top:20px;">
-            <div class="col-sm-4" style="text-align: center;">
-                <img src="images/samsungPhone.jpg" width=50% alt="" class="img-fluid" > 
-                <p>SAMSUNG</p>
-            </div>
-             <div class="col-sm-4" style="text-align: center">
-                <img src="{{ asset('images/xiaomiPhone.jpg')}}" width=50% alt="" class="img-fluid"> 
-                <p>XIAOMI</p>           
-            </div>
-            <div class="col-sm-4" style="text-align: center" >
-                <img src="{{ asset('images/vivoPhone.jpg')}}" width=50% alt="" class="img-fluid" > 
-                <p>VIVO</p>
-            </div>                
+        <div class="col-sm-4" style="text-align: center">
+            <img src="{{ asset('images/xiaomiPhone.jpg')}}" width=50% alt="" class="img-fluid"> 
+            <p>XIAOMI</p>           
         </div>
-            
+        <div class="col-sm-4" style="text-align: center" >
+            <img src="{{ asset('images/vivoPhone.jpg')}}" width=50% alt="" class="img-fluid" > 
+            <p>VIVO</p>
+        </div>                
+    </div>            
+</div>
+<div class="container-fluid">
+    <div class="copyright text-center">
+         &copy; Copyright <strong>ABC company</strong>. All Rights Reserved
     </div>
-
-    <div class="container-fluid">
-            <div class="copyright text-center">
-              &copy; Copyright <strong>ABC company</strong>. All Rights Reserved
-            </div>
-            <div class="credits text-center">
-              Designed by <a href="https://ABC.com/">ABC Company</a>
-            </div>
+    <div class="credits text-center">
+        Designed by <a href="https://ABC.com/">ABC Company</a>
     </div>
+</div>
 @endsection
