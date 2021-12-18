10774a9cd16d92379e7b51c3601c9f3c00649bdd
eugene573 <eugenefong2002@gmail.com>
2021-12-18 17:29:57 +0800

    newwww

--- cart2021c/resources/views/viewProduct.blade.php
+++ cart2021c/resources/views/viewProduct.blade.php
@@ -4,18 +4,18 @@
     <div class="col-sm-2"></div>
     <div class="col-sm-8">
         <br><br>
-        <h3>Products</h3>
+        <h3>  <center>Product List</center> </h3>
         <div class="row">    
             @foreach($products as $product)
             <div class="col-sm-4">
-            <div class="card" style="width: 22rem;">
-                <img class="card-img-top img-fluid" src="{{asset('images/'.$product->image)}}" alt="Product Image" style='max-height: 250px;'>
+            <div class="card" style="width: 19rem;">
+                <img class="card-img-top img-fluid" src="{{asset('images/'.$product->image)}}" alt="Product Image" style='max-height: 350px;'>
                 <div class="card-body">
                     <h5 class="card-title" style="text-align: center;">{{$product->name}}</h5>
                     <p class="card-text">{{$product->description}}</p>
                     <br>
-                    <h5 class="card-text" style="text-align: center;">Price: RM {{$product->price}}</h5>
-                    <a href="{{ route('product.detail', $product->id) }}" class="btn btn-primary" style="margin-left:125px">View</a>
+                    <h5 class="card-text" style="text-align: center;">RM{{$product->price}}</h5>
+                    <a href="{{ route('product.detail', $product->id) }}" class="btn btn-success" style="margin-left:100px">View</a>
                 </div>
             </div>
             </div>
